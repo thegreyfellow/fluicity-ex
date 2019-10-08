b6f1ea24b76bb148ce445c3220574fd96a51ab57
Mac <mac@Mac.local>
2019-10-08 20:53:17 +0100

    Update CardBottom.
    - Finish wide layout.
    - Work on responsive [WIP].

--- src/pages/Home/components/Banner/CardBottom.jsx
+++ src/pages/Home/components/Banner/CardBottom.jsx
@@ -7,33 +7,69 @@ const useStyles = makeStyles(() => ({
   cardBottom: {
     margin: 10,
   },
-  typography: {
-    color: 'red',
+  description: {
+    color: '#5d5d5d',
   },
+  details: {
+    color: '#5d5d5d',
+    fontWeight: 100,
+  },
+  detailsNumber: {
+    color: '#5d5d5d',
+  },
+}));
+
+const buttonStyles = makeStyles(() => ({
   cardButtons: {
+  },
+  button: {
+    borderRadius: 20,
+    width: 150,
+  },
+  Primarybutton: {
+    backgroundColor: 'rgb(73,59,248)',
+    color: 'white',
+  },
+  SecondaryButton: {
+    backgroundColor: 'white',
+    color: 'rgb(73,59,248)',
   },
 }));
 
 const CardBottom = () => {
   const classes = useStyles();
+  const buttonClasses = buttonStyles();
 
   const Buttons = () => (
     <Grid
       xs={12}
-      md={6}
+      md={4}
       item
       container
+      spacing={2}
       direction="row"
       justify="center"
       alignItems="center"
-      className={classes.cardButtons}
+      className={buttonClasses.cardButtons}
     >
-      <Button variant="contained" color="primary">
-        S&apos;abonner
-      </Button>
-      <Button variant="contained" color="primary">
-        Plus d&apos;infos
-      </Button>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="primary"
+          className={`${buttonClasses.button} ${buttonClasses.Primarybutton}`}
+        >
+          S&apos;abonner
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="primary"
+          className={`${buttonClasses.button} ${buttonClasses.SecondaryButton}`}
+        >
+          Plus d&apos;infos
+        </Button>
+      </Grid>
     </Grid>
   );
 
@@ -50,7 +86,7 @@ const CardBottom = () => {
       <Buttons />
       <Grid
         xs={12}
-        md={6}
+        md={8}
         item
         container
         spacing={5}
@@ -59,16 +95,56 @@ const CardBottom = () => {
         alignItems="flex-start"
       >
         <Grid item>
-          <Typography variant="body2" className={classes.cardBottom.typography}>
+          <Typography variant="body2" className={classes.description}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Consequuntur amet nostrum expedita aliquam, officia, exercitationem vitae
             quia consequatur blanditiis placeat vel eos?
           </Typography>
         </Grid>
-        <Grid item container>
-          <Grid item>1800 Citoyens engagés</Grid>
-          <Grid item>569 Contributions citoyennes</Grid>
-          <Grid item>12 Idées conctétisées</Grid>
+        {/* spacing should be 0 when reaching 500px width */}
+        <Grid item container spacing={2}>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item>
+                <Typography variant="subtitle2" className={classes.detailsNumber}>
+                  1800
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" className={classes.details}>
+                  Citoyens engagés
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item>
+                <Typography variant="subtitle2" className={classes.detailsNumber}>
+                  569
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" className={classes.details}>
+                  Contributions citoyennes
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item>
+                <Typography variant="subtitle2" className={classes.detailsNumber}>
+                  12
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" className={classes.details}>
+                  Idées concrétisées
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
